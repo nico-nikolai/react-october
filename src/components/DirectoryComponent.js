b70f9c99995e40e87bbcd2e957f7378979443e09
Nick Rose <nickfbp@gmail.com>
2020-10-17 11:37:34 -0600

    Workshop Week 2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,11 +3,9 @@ import {
   Card,
   CardImg,
   CardImgOverlay,
-  CardText,
-  CardBody,
   CardTitle,
 } from "reactstrap";
-import { CAMPSITES } from "../shared/campsites";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Directory extends Component {
   constructor(props) {
@@ -21,19 +19,6 @@ class Directory extends Component {
       this.setState({selectedCampsite: campsite});
   }
 
-  renderSelectedCampsite(campsite) {
-      if (campsite) {
-          return (
-              <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardTitle>{campsite.name}</CardTitle>
-                    <CardText>{campsite.description}</CardText>
-              </Card>
-          )
-      } else {
-          return <div />
-      }
-  }
 
   render() {
     const directory = this.props.campsites.map((campsite) => {
@@ -52,11 +37,7 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <div className="row">
-            <div className="col-md-5 m-1">
-                {this.renderSelectedCampsite(this.state.selectedCampsite)}
-            </div>
-        </div>
+        <CampsiteInfo campsite={this.state.selectedCampsite}/>
       </div>
     );
   }

139eb0bd8652fa457e3939173f451f2b2e8bb7d6
Nick Rose <nickfbp@gmail.com>
2020-10-23 10:31:08 -0600

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -5,14 +5,17 @@ import {
   CardImgOverlay,
   CardTitle,
 } from "reactstrap";
+import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
   return (
       <Card>
-        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardImgOverlay>
-          <CardTitle>{campsite.name}</CardTitle>
-        </CardImgOverlay>
+        <Link to={`/directory/${campsite.id}`}>
+          <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+          <CardImgOverlay>
+            <CardTitle>{campsite.name}</CardTitle>
+          </CardImgOverlay>
+        </Link>
       </Card>
   );
 }

1c360cac06752a87855edb738b41ae8ad9feceab
Nick Rose <nickfbp@gmail.com>
2020-10-31 11:41:12 -0600

    Workshop Weed 4

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -14,7 +14,7 @@ const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
 const isNumber = val => !isNaN(+val);
-const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2, 4}$/i.test(val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 class Contact extends Component {
   constructor(props) {
     super(props);
@@ -207,7 +207,7 @@ class Contact extends Component {
                   component="div"
                   messages={{
                     required: 'Required',
-                    validEmail: 'Invalid email address'
+                    validEmail: 'Invalid email'
                   }}
                 />
                 </Col>

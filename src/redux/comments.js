f403ef7c639813ce797d0f81eb5904783e1d6d59
Nick Rose <nickfbp@gmail.com>
2020-11-05 16:44:32 -0700

    Fetch Post Comment

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -8,8 +8,6 @@ import * as ActionTypes from './ActionTypes';
             return {...state, errMess: action.payload};
         case ActionTypes.ADD_COMMENT:
             const comment = action.payload;
-            comment.id = state.comments.length;
-            comment.date = new Date().toISOString();
             return {...state, comments: state.comments.concat(comment)};
         default:
             return state;
